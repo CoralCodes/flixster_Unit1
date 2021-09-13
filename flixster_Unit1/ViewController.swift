09cf4fcf91a03e305498d7f5fd381c8a69f8be51
CoralCodes <72894280+CoralCodes@users.noreply.github.com>
2021-09-12 22:28:42 -0400

    Initial Commit

---
+++ flixster_Unit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster_Unit1
+//
+//  Created by Coral Hidalgo on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
