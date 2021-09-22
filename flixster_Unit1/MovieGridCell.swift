74df83bf1c941a98bfb56b6a3b15ff3a3c5087f1
CoralCodes <72894280+CoralCodes@users.noreply.github.com>
2021-09-21 23:18:55 -0400

    Grid layout

---
+++ flixster_Unit1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster_Unit1
+//
+//  Created by Coral Hidalgo on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
